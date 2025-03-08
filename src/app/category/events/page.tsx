c9c06046e3fbe99518033a0ea43a6f494a32183e
satish7654 <satishsingh202dd+@gmail.com>
2025-03-08 16:30:01 +0545

    test done

---
+++ src/app/category/events/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      this is events section
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/category/news/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      this is news section
+    </div>
+  )
+}
+
+export default page
